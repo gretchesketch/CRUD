b5e4996ece49db373b83e1b4f10166ef09bef2a7
gretchesketch <gretchenjensen92@gmail.com>
2022-06-09 17:34:12 -0400

    updated connection and server js

--- server.js
+++ server.js
@@ -1,43 +1,72 @@
-//dependencies
-const express = require("express");
-// cors is middleware for express
-// and a node.js package
-var cors = require('cors');
-var app = express();
-const dotenv = require("dotenv");
-const { response } = require("express");
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
 dotenv.config();
+
+const dbService = require('./dbService');
 
 app.use(cors());
 app.use(express.json());
-// set to false because we will not be sending in any form data.
-app.use(express.urlencoded({ extended : false}));
+app.use(express.urlencoded({ extended : false }));
 
-//routes
-//create
-app.post("/insert", (req, res) => {
 
-});
-
-//read
-app.get("/getAll", (req, res) => {
-    response.json({
-        success: true
-    });
-});
-
-//update
-app.post("/", (req, res) => {
-
-});
-
-//delete
-app.get("/", (req, res) => {
+// create
+app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
     
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({ data: data}))
+    .catch(err => console.log(err));
 });
 
-//app listening on PORT
-app.listen(process.env.PORT, () => console.log("app is running"));
+// read
+app.get('/getAll', (request, response) => {
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+// update
+app.patch('/update', (request, response) => {
+    const { id, name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.updateNameById(id, name);
+    
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+});
+
+// delete
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+    
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+});
+
+app.get('/search/:name', (request, response) => {
+    const { name } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.searchByName(name);
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+app.listen(process.env.PORT, () => console.log('app is running'));
